a4dccf9950931bb44b662225a2181fc822eafc24
shreedharSRG <shreegavali2@gmail.com>
2026-07-24 16:54:23 +0530

    Add frontend practice files for HTML, CSS, Grid, Flexbox, Forms, and Tables

---
+++ practice/basic.js
@@ -0,0 +1,15 @@
+function changeText()
+{ document.getElementById('demo').innerHTML="para is changed"
+    document.getElementById('1').innerHTML="changed"
+}
+    function Sum(a,b)
+    {
+        return a+b;
+
+    }
+    console.log(Sum(1,2));
+
+    
+
+
+
