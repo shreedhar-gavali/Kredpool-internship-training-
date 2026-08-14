d1234b1edb445fb81f88522579d69ab5d077ee9a
shreedharSRG <shreegavali2@gmail.com>
2026-08-14 16:21:59 +0530

    add the questions on string

---
+++ practice/palindrom.js
@@ -0,0 +1,21 @@
+
+let name1="abba";
+let reverse="";
+for(let i=name1.length-1;i>=0;i--){
+   reverse = reverse+ name1[i];
+   
+}
+if(reverse===name1){
+    console.log("its palindrom");
+}
+else{
+    console.log("its not palindrom");
+}
+
+
+
+
+
+
+
+
--- practice/postMethod.js
+++ practice/postMethod.js
@@ -13,10 +13,39 @@ async function postdata(){
 
       body:JSON.stringify(user)});
 
+    
     const data =await response.json();
     console.log(response.status);
     console.log(data);
- return data;
+ return data; 
 }
-postdata();
+// postdata();
+
+async function updatedata(id)
+{
+   const user={
     
+    email:"shree2@gmail.com"
+   }
+
+
+  const  response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{method:"PATCH",
+    headers:{"Content-Type":"application/json"},
+    body: JSON.stringify(user)
+   
+  })
+  console.log("status", response.status);
+  const  redata= await response.json();
+
+  console.log("updated data",redata);
+  return redata;
+
+}    
+// updatedata();
+
+const main = async()=>
+{
+ const createdData = await postdata();
+ const updatedData = await updatedata(createdData.id);
+}
+main();
